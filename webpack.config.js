c90425d554e262ee188c65861585d283f5d6e7c6
andreev-oe <andreev.oe@yandex.ru>
2022-08-21 00:25:59 +0300

    feat: add framework in master and refactor - add framework into master - merge master into branch module4-task1 for saving refactor commits - add style-loader and css loader in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-        }
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        exclude: /(node_modules)/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
