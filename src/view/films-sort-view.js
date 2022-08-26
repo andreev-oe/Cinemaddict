aee7757276dae04ac5d3c8772d124035d058c538
andreev-oe <andreev.oe@yandex.ru>
2022-08-27 00:53:14 +0300

    feat: add film sort logic

--- src/view/films-sort-view.js
+++ src/view/films-sort-view.js
@@ -8,37 +8,67 @@ const createSortElement = () => `<ul class="sort">
 
 export default class FilmsSortView extends AbstractView {
   #films = null;
+  #defaultSortButton = null;
+  #dateSortButton = null;
+  #ratingSortButton = null;
+  #sortButtons = null;
+  #activeButtonClass = 'sort__button--active';
 
   constructor(films) {
     super();
     this.#films = films;
+    this.#defaultSortButton = this.element.querySelector('.sort__button--default');
+    this.#dateSortButton = this.element.querySelector('.sort__button--date');
+    this.#ratingSortButton = this.element.querySelector('.sort__button--rating');
+    this.#sortButtons = this.element.children;
   }
 
   get template () {
     return createSortElement();
   }
 
+  #removeActiveButtonClass = () => {
+    for (const sortButton of this.#sortButtons) {
+      if (sortButton.children[0].classList.contains('sort__button--active')) {
+        sortButton.children[0].classList.remove('sort__button--active');
+        return;
+      }
+    }
+  };
+
   setSortButtonsHandlers = (defaultSort, daySort, ratingSort) => {
     this._callback.defaultSort = defaultSort;
     this._callback.daySort = daySort;
     this._callback.ratingSort = ratingSort;
-    this.element.querySelector('.sort__button--default').addEventListener('click', this.#onDefaultSortButtonClick);
-    this.element.querySelector('.sort__button--date').addEventListener('click', this.#onDaySortButtonClick);
-    this.element.querySelector('.sort__button--rating').addEventListener('click', this.#onRatingSortButtonClick);
+    this.#defaultSortButton.addEventListener('click', this.#onDefaultSortButtonClick);
+    this.#dateSortButton.addEventListener('click', this.#onDaySortButtonClick);
+    this.#ratingSortButton.addEventListener('click', this.#onRatingSortButtonClick);
   };
 
   #onDefaultSortButtonClick = (evt) => {
     evt.preventDefault();
-    this._callback.defaultSort(this.#films);
+    if (this.#defaultSortButton.classList.contains(this.#activeButtonClass)) {
+      return;
+    }
+    this.#removeActiveButtonClass();
+    this._callback.defaultSort(this.#films, this.#defaultSortButton, this.#activeButtonClass);
   };
 
   #onDaySortButtonClick = (evt) => {
     evt.preventDefault();
-    this._callback.daySort(this.#films);
+    if (this.#dateSortButton.classList.contains(this.#activeButtonClass)) {
+      return;
+    }
+    this.#removeActiveButtonClass();
+    this._callback.daySort(this.#films, this.#dateSortButton, this.#activeButtonClass);
   };
 
   #onRatingSortButtonClick = (evt) => {
     evt.preventDefault();
-    this._callback.ratingSort(this.#films);
+    if (this.#ratingSortButton.classList.contains(this.#activeButtonClass)) {
+      return;
+    }
+    this.#removeActiveButtonClass();
+    this._callback.ratingSort(this.#films, this.#ratingSortButton, this.#activeButtonClass);
   };
 }
